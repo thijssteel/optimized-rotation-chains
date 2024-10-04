0d0e7cee0c3024f9c3a436423f62e65f5030cb3f
thijssteel <thijs.steel@kuleuven.be>
2024-10-04 16:45:03 -0600

    make avx kernel a bit more general

--- optimized/kernels_avx2_double.c
+++ optimized/kernels_avx2_double.c
@@ -42,48 +42,48 @@ void drotc_kernel_12xnx3(int n,
         c_vec = _mm256_set1_pd(P[0]);
         s_vec = _mm256_set1_pd(P[1]);
 
-        temp = _mm256_fmadd_pd(c_vec, a_0_2, _mm256_mul_pd(s_vec, a_0_next));
-        a_0_2 = _mm256_fnmadd_pd(s_vec, a_0_2, _mm256_mul_pd(c_vec, a_0_next));
+        temp = c_vec * a_0_2 + s_vec * a_0_next;
+        a_0_2 = -s_vec * a_0_2 + c_vec * a_0_next;
         a_0_next = temp;
 
-        temp = _mm256_fmadd_pd(c_vec, a_1_2, _mm256_mul_pd(s_vec, a_1_next));
-        a_1_2 = _mm256_fnmadd_pd(s_vec, a_1_2, _mm256_mul_pd(c_vec, a_1_next));
+        temp = c_vec * a_1_2 + s_vec * a_1_next;
+        a_1_2 = -s_vec * a_1_2 + c_vec * a_1_next;
         a_1_next = temp;
 
-        temp = _mm256_fmadd_pd(c_vec, a_2_2, _mm256_mul_pd(s_vec, a_2_next));
-        a_2_2 = _mm256_fnmadd_pd(s_vec, a_2_2, _mm256_mul_pd(c_vec, a_2_next));
+        temp = c_vec * a_2_2 + s_vec * a_2_next;
+        a_2_2 = -s_vec * a_2_2 + c_vec * a_2_next;
         a_2_next = temp;
 
         // Apply rotation 1 to values 1 and 2 (next)
         c_vec = _mm256_set1_pd(P[2 + 0]);
         s_vec = _mm256_set1_pd(P[2 + 1]);
 
-        temp = _mm256_fmadd_pd(c_vec, a_0_1, _mm256_mul_pd(s_vec, a_0_next));
-        a_0_1 = _mm256_fnmadd_pd(s_vec, a_0_1, _mm256_mul_pd(c_vec, a_0_next));
+        temp = c_vec * a_0_1 + s_vec * a_0_next;
+        a_0_1 = -s_vec * a_0_1 + c_vec * a_0_next;
         a_0_next = temp;
 
-        temp = _mm256_fmadd_pd(c_vec, a_1_1, _mm256_mul_pd(s_vec, a_1_next));
-        a_1_1 = _mm256_fnmadd_pd(s_vec, a_1_1, _mm256_mul_pd(c_vec, a_1_next));
+        temp = c_vec * a_1_1 + s_vec * a_1_next;
+        a_1_1 = -s_vec * a_1_1 + c_vec * a_1_next;
         a_1_next = temp;
 
-        temp = _mm256_fmadd_pd(c_vec, a_2_1, _mm256_mul_pd(s_vec, a_2_next));
-        a_2_1 = _mm256_fnmadd_pd(s_vec, a_2_1, _mm256_mul_pd(c_vec, a_2_next));
+        temp = c_vec * a_2_1 + s_vec * a_2_next;
+        a_2_1 = -s_vec * a_2_1 + c_vec * a_2_next;
         a_2_next = temp;
 
         // Apply rotation 2 to values 0 and 1 (next)
         c_vec = _mm256_set1_pd(P[2 * 2 + 0]);
         s_vec = _mm256_set1_pd(P[2 * 2 + 1]);
 
-        temp = _mm256_fmadd_pd(c_vec, a_0_0, _mm256_mul_pd(s_vec, a_0_next));
-        a_0_0 = _mm256_fnmadd_pd(s_vec, a_0_0, _mm256_mul_pd(c_vec, a_0_next));
+        temp = c_vec * a_0_0 + s_vec * a_0_next;
+        a_0_0 = -s_vec * a_0_0 + c_vec * a_0_next;
         a_0_next = temp;
 
-        temp = _mm256_fmadd_pd(c_vec, a_1_0, _mm256_mul_pd(s_vec, a_1_next));
-        a_1_0 = _mm256_fnmadd_pd(s_vec, a_1_0, _mm256_mul_pd(c_vec, a_1_next));
+        temp = c_vec * a_1_0 + s_vec * a_1_next;
+        a_1_0 = -s_vec * a_1_0 + c_vec * a_1_next;
         a_1_next = temp;
 
-        temp = _mm256_fmadd_pd(c_vec, a_2_0, _mm256_mul_pd(s_vec, a_2_next));
-        a_2_0 = _mm256_fnmadd_pd(s_vec, a_2_0, _mm256_mul_pd(c_vec, a_2_next));
+        temp = c_vec * a_2_0 + s_vec * a_2_next;
+        a_2_0 = -s_vec * a_2_0 + c_vec * a_2_next;
         a_2_next = temp;
 
         // Store value 0 (stored in next)
@@ -139,16 +139,16 @@ void drotc_kernel_12xnx1(int n,
         c_vec = _mm256_set1_pd(P[0]);
         s_vec = _mm256_set1_pd(P[1]);
 
-        temp1 = _mm256_fmadd_pd(c_vec, a_0_0, _mm256_mul_pd(s_vec, a_0_next));
-        a_0_0 = _mm256_fnmadd_pd(s_vec, a_0_0, _mm256_mul_pd(c_vec, a_0_next));
+        temp1 = c_vec * a_0_0 + s_vec * a_0_next;
+        a_0_0 = -s_vec * a_0_0 + c_vec * a_0_next;
         a_0_next = temp1;
 
-        temp2 = _mm256_fmadd_pd(c_vec, a_1_0, _mm256_mul_pd(s_vec, a_1_next));
-        a_1_0 = _mm256_fnmadd_pd(s_vec, a_1_0, _mm256_mul_pd(c_vec, a_1_next));
+        temp2 = c_vec * a_1_0 + s_vec * a_1_next;
+        a_1_0 = -s_vec * a_1_0 + c_vec * a_1_next;
         a_1_next = temp2;
 
-        temp3 = _mm256_fmadd_pd(c_vec, a_2_0, _mm256_mul_pd(s_vec, a_2_next));
-        a_2_0 = _mm256_fnmadd_pd(s_vec, a_2_0, _mm256_mul_pd(c_vec, a_2_next));
+        temp3 = c_vec * a_2_0 + s_vec * a_2_next;
+        a_2_0 = -s_vec * a_2_0 + c_vec * a_2_next;
         a_2_next = temp3;
 
         // Store value 0 (stored in next)
